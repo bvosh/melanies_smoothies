b0ee936b00284dc6d70888b4642e7f947cd78fb8
bvosh <brandon.voshall@trulieve.com>
2026-04-18 19:15:51 -0400

    Fetch and display watermelon nutrition information
    Added functionality to fetch and display nutrition information for watermelon from Smoothiefroot API.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages.
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests 
 
 # Write directly to the app.
 st.title("🥤Customize Your Smoothie!")
@@ -23,11 +24,12 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
 
@@ -45,8 +47,4 @@ if ingredients_list:
         else:
             st.success('Your Smoothie is ordered!', icon="✅")
 
-# new section to display smoothiefroot nutrition information
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
